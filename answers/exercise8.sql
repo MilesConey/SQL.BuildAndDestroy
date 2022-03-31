7f903dbb4770af20ec187e3259ba3f0bb2b3e481
Miles Coney <MilesEConey@gmail.com>
2022-03-31 14:16:31 -0400

    Completed 8 and 9

--- answers/exercise8.sql
+++ answers/exercise8.sql
@@ -1,4 +1,36 @@
 
 
 
+mysql> create table Students(
+    -> StudentName VARCHAR(50) NOT NULL,
+    -> Address VARCHAR(30),
+    -> City VARCHAR(50),
+    -> PostalCode VARCHAR(10),
+    -> Country VARCHAR(30)
+    -> );
+Query OK, 0 rows affected (0.05 sec)
 
+mysql>
+mysql> show tables;
++-------------------+
+| Tables_in_mynewdb |
++-------------------+
+| Students          |
+| Users             |
++-------------------+
+2 rows in set (0.01 sec)
+
+mysql> Describe Students;
++-------------+-------------+------+-----+---------+-------+
+| Field       | Type        | Null | Key | Default | Extra |
++-------------+-------------+------+-----+---------+-------+
+| StudentName | varchar(50) | NO   |     | NULL    |       |
+| Address     | varchar(30) | YES  |     | NULL    |       |
+| City        | varchar(50) | YES  |     | NULL    |       |
+| PostalCode  | varchar(10) | YES  |     | NULL    |       |
+| Country     | varchar(30) | YES  |     | NULL    |       |
++-------------+-------------+------+-----+---------+-------+
+5 rows in set (0.00 sec)
+
+ insert into Students Values ('Jane Doe', '57 Union St', 'Glasgow', 'G13RB', 'Scotland');
+Query OK, 1 row affected (0.02 sec)
---
+++ answers/exercise9.sql
@@ -0,0 +1,8 @@
+mysql> SELECT * FROM Students WHERE city <> 'Philadelphia';
++-------------+--------------------+---------+------------+----------------+
+| StudentName | Address            | City    | PostalCode | Country        |
++-------------+--------------------+---------+------------+----------------+
+| Jane Doe    | 57 Union St        | Glasgow | G13RB      | Scotland       |
+| John Doe    | 123 Street St      | Boston  | 12366      |  United States |
+| Stacy Heart | 133 Bleaker Street | London  | NULL       | NULL           |
++-------------+--------------------+---------+------------+----------------+
